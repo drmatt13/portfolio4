74fb01c8601308ba83a8b31c23091f95bed45137
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-06-03 00:29:31 -0400

    test

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 
 const Navbar = ({redirect, modal, setModal}) => {
 
-  const [mobile] = useState( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+  // const [mobile] = useState( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 
   // log mobile status
-  useEffect(() => {
-    console.log(mobile);
-  }, [mobile])
+  // useEffect(() => {
+  //   console.log(mobile);
+  // }, [mobile])
 
   const toggleModal = (s) => {
     modal === s ? setModal(null) : setModal(s)
   }
 
   return (
-    <div className={`master-navbar-container ${mobile ? "navbar-mobile" : "navbar-web"}`}>
+    // <div className={`master-navbar-container ${mobile ? "navbar-mobile" : "navbar-web"}`}>
+    <div className="master-navbar-container">
       <div className="navbar-container">
         <div className="navbar-btn" onClick={() => redirect("/")}>
           <i className="fas fa-home"></i>
