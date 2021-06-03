70d6e83be995f7e399a60eb4bedbdcf56abe653d
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-06-03 02:24:14 -0400

    sfksdfsdf

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 
 const Navbar = ({mobile, redirect, modal, setModal}) => {
