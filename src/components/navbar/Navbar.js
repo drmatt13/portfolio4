5f664b931bfdc1bf292968a64890d4565163e9db
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-06-03 10:35:15 -0400

    2 bars

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-// import React, { useState, useEffect } from 'react';
+// import { useCallback } from 'react';
 import './Navbar.css';
 
 const Navbar = ({mobile, redirect, modal, setModal}) => {
@@ -7,25 +7,24 @@ const Navbar = ({mobile, redirect, modal, setModal}) => {
     modal === s ? setModal(null) : setModal(s)
   }
 
+  // const touch
+
   return (
     <div className={`master-navbar-container ${mobile ? "navbar-mobile" : "navbar-web"}`}>
-    {/* <div className="master-navbar-container"> */}
-      <div className="navbar-container">
-        <div className="navbar-btn" onClick={() => redirect("/")}>
-          <i className="fas fa-home"></i>
-        </div>
-        <div className="navbar-btn" onClick={() => toggleModal("examples")}>
-          <i className="fas fa-laptop-code"></i>
-        </div>
-        <div className="navbar-btn" onClick={() => toggleModal("contact")}>
-          <i className="far fa-address-card"></i>
-        </div>
-        <div className="navbar-btn" onClick={() => toggleModal("about")}>
-          <i className="fas fa-question"></i>
-        </div>
-        <div className="navbar-btn" onClick={() => toggleModal("settings")}>
-          <i className="fas fa-cog"></i>
-        </div>
+      <div className="navbar-btn" onClick={() => redirect("/")}>
+        <i className="fas fa-home"></i>
+      </div>
+      <div className="navbar-btn" onClick={() => toggleModal("examples")}>
+        <i className="fas fa-laptop-code"></i>
+      </div>
+      <div className="navbar-btn" onClick={() => toggleModal("contact")}>
+        <i className="far fa-address-card"></i>
+      </div>
+      <div className="navbar-btn" onClick={() => toggleModal("about")}>
+        <i className="fas fa-question"></i>
+      </div>
+      <div className="navbar-btn" onClick={() => toggleModal("settings")}>
+        <i className="fas fa-cog"></i>
       </div>
     </div>
   )
