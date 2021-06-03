b2dd20f96b0c2a535d05818ddfd3093dd4eeb672
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-06-03 02:19:43 -0400

    seriously

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 // components
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import ReactApp from './components/react app/ReactApp';
-import Modal from './components/ui/Modal';
+// import Navbar from './components/navbar/Navbar';
+// import Home from './components/home/Home';
+// import ReactApp from './components/react app/ReactApp';
+// import Modal from './components/ui/Modal';
 
 // example react apps
-const examples = ['Path_Finder', 'Todos'];
+// const examples = ['Path_Finder', 'Todos'];
 
 // check if mobile device
 const mobile = /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
